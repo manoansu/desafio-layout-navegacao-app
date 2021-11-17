d20e0b702a3d324b5d8e09c4ad0a2fb8561f8f88
Ansumane Mané <manoansu@gmail.com>
2021-11-17 21:43:17 +0000

    fixing style

--- desafio-layout-navegacao-app/src/components/CarCard/index.tsx
+++ desafio-layout-navegacao-app/src/components/CarCard/index.tsx
@@ -1,5 +1,4 @@
 import './styles.css';
-import { ReactComponent as SearchCarIcon } from '../../assets/images/buscacar.svg';
 import CarImg from '../../assets/images/car.png';
 import ButtonBuy from '../ButtonBuy';
 
@@ -7,19 +6,18 @@ const CarCard = () =>{
 
     return (
         <div className="home-car">
-
-        <div className="base-card car_card">
-            <div className="card-top-container">
-                <img src={ CarImg } alt="Car" />
+            <div className="base-card car_card">
+                <div className="card-top-container">
+                    <img src={ CarImg } alt="Car" />
+                </div>
+                <div className="car-doc-container">
+                    <h4>Audi Supra TT</h4>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cupiditate, nisi</p>
+                </div>
+                <div className="btn-car">
+                    <ButtonBuy />
+                </div>
             </div>
-            <div className="car-doc-container">
-                <h4>Audi Supra TT</h4>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cupiditate, nisi</p>
-            </div>
-            <div className="btn-car">
-                <ButtonBuy />
-            </div>
-        </div>
         </div>
     );
 };
--- desafio-layout-navegacao-app/src/page/Catalog/index.tsx
+++ desafio-layout-navegacao-app/src/page/Catalog/index.tsx
@@ -1,9 +1,22 @@
 import CarCard from "../../components/CarCard";
-
+import { ReactComponent as SearchCarIcon } from '../../assets/images/buscacar.svg';
 const Catalog = () =>{
 
     return(
-        <div className="container-my-4">
+        <div className="container my-4">
+            <div className="base-card search-car">
+                <div className="input-group col-md-4">
+                    <input className="form-control py-2" type="search" value="" placeholder="Digite sua busca" id="example-search-input"/>
+                    <span className="input-group-append">
+                        <button className="btn btn-outline-secondary" type="button">
+                            <i className="fa fa-search"></i>
+                        </button>
+                    </span>
+                    <div  className="car-icon-container">
+                        <SearchCarIcon />
+                    </div>
+                </div>
+            </div>
             <div className="row">
             <div className="col-sm-6 col-lg-4 col-xl-3">
                     <CarCard />
